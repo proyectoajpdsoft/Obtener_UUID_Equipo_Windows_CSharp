df16a22578d31fbf4edbaf143c5634c041c4162e
proyectoajpdsoft <alonsojpd@gmail.com>
2025-03-10 23:15:18 +0100

    Obtener Identificador UUID MID con CSharp
    Cómo obtener el Identificador de dispositivo de Windows (UUID o MID) con Visual Sutio .Net C# (C Sharp). Ejemplo en C# de acceso al Registro de configuración de Windows para leer un valor de una clave.

---
+++ formUUID.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ObtenerUUID
+{
+    partial class formObtenerUUID
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btObtenerUUID = new System.Windows.Forms.Button();
+            this.txtUUID = new System.Windows.Forms.TextBox();
+            this.btCopiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btObtenerUUID
+            // 
+            this.btObtenerUUID.Location = new System.Drawing.Point(12, 17);
+            this.btObtenerUUID.Name = "btObtenerUUID";
+            this.btObtenerUUID.Size = new System.Drawing.Size(91, 29);
+            this.btObtenerUUID.TabIndex = 0;
+            this.btObtenerUUID.Text = "Obtener UUID";
+            this.btObtenerUUID.UseVisualStyleBackColor = true;
+            this.btObtenerUUID.Click += new System.EventHandler(this.btObtenerUUID_Click);
+            // 
+            // txtUUID
+            // 
+            this.txtUUID.Location = new System.Drawing.Point(109, 22);
+            this.txtUUID.Name = "txtUUID";
+            this.txtUUID.Size = new System.Drawing.Size(382, 20);
+            this.txtUUID.TabIndex = 1;
+            // 
+            // btCopiar
+            // 
+            this.btCopiar.Location = new System.Drawing.Point(503, 22);
+            this.btCopiar.Name = "btCopiar";
+            this.btCopiar.Size = new System.Drawing.Size(54, 23);
+            this.btCopiar.TabIndex = 2;
+            this.btCopiar.Text = "Copiar";
+            this.btCopiar.UseVisualStyleBackColor = true;
+            this.btCopiar.Click += new System.EventHandler(this.btCopiar_Click);
+            // 
+            // formObtenerUUID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 70);
+            this.Controls.Add(this.btCopiar);
+            this.Controls.Add(this.txtUUID);
+            this.Controls.Add(this.btObtenerUUID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "formObtenerUUID";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Obtener Identificador de dispositivo - ProyectoA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btObtenerUUID;
+        private System.Windows.Forms.TextBox txtUUID;
+        private System.Windows.Forms.Button btCopiar;
+    }
+}
+
---
+++ formUUID.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace ObtenerUUID
+{
+    public partial class formObtenerUUID : Form
+    {
+        // Obtiene el Identificador de dispositivo en equipo Windows leyendo clave del registro
+        public string obtenerUUID()
+        {
+            string uuid;
+
+            try
+            {
+                //Accedemos a la clave de registro x64 que contiene el dato del UUID MID del equipo
+                RegistryKey clave = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\SQMClient");
+                //Obtenemos su valor, si es nulo lo comprobamos para evitar errores indeseados
+                if (clave != null && clave.GetValue("MachineId") != null)
+                {
+                    uuid = clave.GetValue("MachineId").ToString();
+                    // Quitamos las llaves que suele llevar de inicio y de cierre
+                    uuid = uuid.Replace("{", "").Replace("}", "");
+                }
+                else
+                    uuid = "";
+                return uuid;
+            }
+            catch (Exception error)
+            {
+                string mensaje = $"Error al obtener el UUID (MID) del equipo: {error.Message}";
+                MessageBox.Show(mensaje,"Error al obtener UUID MID...",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "";
+            }
+        }
+
+        public formObtenerUUID()
+        {
+            InitializeComponent();
+        }
+
+        private void btObtenerUUID_Click(object sender, EventArgs e)
+        {
+            string uuid = "";
+            uuid = obtenerUUID();
+            txtUUID.Text = uuid;
+        }
+
+        private void btCopiar_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtUUID.Text);
+        }
+    }
+}
